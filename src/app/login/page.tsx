be4647d97c63bbf6f0f229cdc2bbcdb21f814bfd
Carlos Alessandro <carllosalessandro@gmail.com>
2025-10-05 12:06:24 -0300

    feat: criando pagina de login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,11 +1,17 @@
 'use client';
 import { use, useState } from 'react';
+import styles from './login.module.css';
+import { Eye, EyeSlash } from 'phosphor-react';
+import { Button } from '@/components/Button';
+import { Input } from '@/components/Input';
+import { FormField } from '@/components/FormField';
 
 export default function LoginPage() {
   const [formData, setFormData] = useState({
     email: '',
     password: '',
   });
+  const [showPassword, setShowPassword] = useState(false);
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setFormData({
@@ -20,15 +26,42 @@ export default function LoginPage() {
   };
 
   return (
-    <div>
-      <div>
+    <div className={styles.container_login}>
+      <div className={styles.container_form}>
         <h1>Login</h1>
         <form onSubmit={handleSubmit}>
-          <label htmlFor="email">Email:</label>
-          <input type="email" id="email" name="email" onChange={handleChange} />
-          <label htmlFor="password">Password:</label>
-          <input type="password" id="password" name="password" onChange={handleChange} />
-          <button type="submit">Login</button>
+          <FormField id="email" label="Email:">
+            <Input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="email@email.com"
+              onChange={handleChange}
+            />
+          </FormField>
+          <FormField id="password" label="Password:">
+            <div className={styles.password_input}>
+              <Input
+                type={showPassword ? 'text' : 'password'}
+                id="password"
+                name="password"
+                placeholder="*********"
+                onChange={handleChange}
+              />
+              <button
+                type="button"
+                onClick={() => setShowPassword(!showPassword)}
+                aria-label={showPassword ? 'Ocultar senha' : 'Mostrar senha'}
+                title={showPassword ? 'Ocultar senha' : 'Mostrar senha'}
+              >
+                {showPassword ? <EyeSlash /> : <Eye />}
+              </button>
+            </div>
+          </FormField>
+          <a href="/cadastro">Cadastrar</a>
+          <Button aria-label="Login" type="submit" title="Login">
+            Login
+          </Button>
         </form>
       </div>
     </div>
